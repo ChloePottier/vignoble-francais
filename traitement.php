960e2b20a97b98aedebec22e0cfd204f5a692406
ChloePottier <chloe.pottier@free.fr>
2019-12-29 14:08:48 +0100

    page traitement

--- traitement.php
+++ traitement.php
@@ -9,15 +9,29 @@
     <meta property="og:image" content="http://exemple.com/article_thumbnail.jpg" />
     <meta property="og:description" content="Cet article présente les vins français" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
     <title>Message envoyé</title>
 </head>
 <body class="bg-color-bordeaux-light color-white">
-    <div class="reception-message">
-        <p>Votre message a bien été envoyé.</p>
-        <a href="index.html"  >
-            <button class="btn-envoyer retour color-white">Home</button>
-        </a>
+<div class="container-fluid">
+    <div class="container">
+        <div class="row pt-5">
+            <div class="col-12">
+            <p class="text-center font-size-16">Votre message a bien été envoyé.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 d-flex justify-content-center">
+                <div class="">
+                    <a href="index.html" class="color-black bg-color-green btn-grey-hover text-decoration-none border-10px text-uppercase font-size-16 border-0 py-2 px-3">Home</a>
+                </div>
+            </div>
+        </div>
+
+
+        
     </div>
+</div>
 </body>
 </html>
